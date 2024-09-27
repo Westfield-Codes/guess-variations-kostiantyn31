9d1b1e5200ce5bc6cab477939ed3c866af052472
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-09-27 16:05:54 +0000

    done

---
+++ done0.75-guess.js
@@ -0,0 +1,23 @@
+/* Simple Guess Program */
+function rollDice(){
+    // create variable called guess, set to 0
+    let guess = 0;
+    // create variable called answer and store a random integer 1-100
+    let answer = Math.floor(Math.random()*100+1);
+    // display the answer for testing purposes only
+    
+    // set guess equal to prompt asking user to "Guess a number, 1-100"
+    guess = prompt("Guess a number, 1-100");
+    // if guess equals answer display "Correct!" 
+    if (guess == answer){
+      alert("Correct!");
+    }
+    // or if guess is greater than answer display "Too high!" 
+    else if (guess > answer){
+      alert("Too high!");
+    }
+    // or display "Too low!" 
+    else if (guess < answer){
+      alert("Too low!");
+    }
+    }
--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-/* Define a function called rollDice guessGame */
-function rollDice(){
-  // create a variable called roll, set it equal to random 1-6
-  let roll = Math.floor(Math.random()*6+1);
-  // if roll equals 6, display "6! You win!"
-if (roll == 6) alert("6! You win!");
-  // use a confirm and say: "You rolled a [roll]"
-else { 
-confirm("You rolled a " + roll);
-rollDice();
-}
-// close the function block
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame(){
+  // generate a random integer 1-100
+  let answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+alert(answer);
+  // create guess and set equal to 0 
+var quess = 0;
+  // loop while guess is not equal to answer
+  
+    // set guess equal to asking user to "Guess a number, 1-100"
+  quess = prompt("Guess a number, 1-100");
+    // if guess equals answer display "Correct!" 
+  if (quess == answer) alert("Correct!");
+    // or if guess is greater than answer display "Too high!" 
+
+    // or if guess is less than answer display "Too low!" 
+
+    // else say "Bad input"
+
+  // end the loop
+
+// end the function
 }
